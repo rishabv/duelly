058aebf9ce10c11ca660ce0761187ef7fa5e3a3d
rishabv <rvbsc99@gmail.com>
2026-01-28 00:17:00 +0530

    chore: async configs added

--- src/main/java/com/duelly/configurations/AsyncConfiguration.java
+++ src/main/java/com/duelly/configurations/AsyncConfiguration.java
@@ -1,4 +1,27 @@
 package com.duelly.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
 public class AsyncConfiguration {
+    @Bean("asyncExecutor")
+    public Executor asyncExecutor() {
+        int minPoolSize = 2;
+        int maxPoolSize = 4;
+        int queueSize = 3;
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueSize);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.setCorePoolSize(minPoolSize);
+        executor.initialize();
+        return executor;
+    }
 }
